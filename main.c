c834e0e67259aa7af0f5d6d57ddb6e76f212e864
Na-yunyoung <yunyoung23@sookmyung.ac.kr>
2023-11-30 15:29:47 +0900

    13-2

--- main.c
+++ main.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-//형식 선언 
-struct student{
-		int ID;//학번 
-		char name[100];//이름 
-		double score;//학점 
+struct point{
+	int	x;
+	int y;
 };
 int main(int argc, char *argv[]) {
 	//instance 선언 
-	struct student s1 = {123,"Yun young", 4.3};
+	struct point p1,p2;
+	int xdiff,ydiff;
+	double dist;
 	
-	//다른 값 변경 
-	s1.ID = 123456;
-	s1.name[0] = 'y';
-	s1.score = 4.2;
+	printf("input p1 coordinate(x,y) : ");
+	scanf("%d %d",&p1.x,&p1.y);
 	
-	//출력
-	printf("ID : %i\n",s1.ID);
-	printf("name: %s\n",s1.name);
-	printf("grade : %f\n",s1.score); 
+	printf("input p2 coordinate(x,y) : ");
+	scanf("%d %d",&p2.x,&p2.y);
 	
-	strcpy(s1.name,"Bongjun");
-	printf("name2 : %s\n",s1.name);
+	//distance
+	xdiff = p2.x - p1.x;
+	ydiff = p2.y - p1.y;
+	dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+	
+	printf("distance : %f\n",dist);
+	 
+
 	return 0;
 }
